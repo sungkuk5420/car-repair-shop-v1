ebeb623570540fed73dd225c8e8025e50bd3fe6e
sungkuk5420 <sungkuk5420@gmail.com>
2021-07-25 15:19:35 +0900

    footer 메뉴 해당 페이지에서 활성화, 비활성화 처리하도록 기능개발

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,33 +1,93 @@
 <template>
   <div class="footer">
-    <div class="footer-button">
-      <img class="button-image-1 off" src="~assets/images/bot_menu.01_off.jpg" alt="" srcset="">
-      <img class="button-image-1 on" src="~assets/images/bot_menu.01_on.jpg" alt="" srcset="">
+    <div class="footer-button" @click="movePage('/main')">
+      <img class="button-image-1 off" 
+        src="~assets/images/bot_menu.01_off.jpg" 
+        :class="footerTabName!=='main'?'is-active':''"
+        alt="" srcset=""
+      />
+      <img class="button-image-1 on" 
+        src="~assets/images/bot_menu.01_on.jpg" 
+        :class="footerTabName==='main'?'is-active':''"
+        alt="" srcset=""
+      />
     </div>
-    <div class="footer-button">
-      <img class="button-image-2 off" src="~assets/images/bot_menu.02_off.jpg" alt="" srcset="">
-      <img class="button-image-2 on" src="~assets/images/bot_menu.02_on.jpg" alt="" srcset="">
+    <div class="footer-button" @click="movePage('/main')">
+      <img class="button-image-2 off" 
+        src="~assets/images/bot_menu.02_off.jpg" 
+        :class="footerTabName!=='main'?'is-active':''"
+        alt="" srcset=""
+      />
+      <img class="button-image-2 on" 
+        src="~assets/images/bot_menu.02_on.jpg" 
+        :class="footerTabName==='main'?'is-active':''"
+        alt="" srcset=""
+      />
     </div>
-    <div class="footer-button">
-      <img class="button-image-3 off" src="~assets/images/bot_menu.03_off.jpg" alt="" srcset="">
-      <img class="button-image-3 on" src="~assets/images/bot_menu.03_on.jpg" alt="" srcset="">
+    <div class="footer-button" @click="movePage('/event')">
+      <img class="button-image-3 off" 
+        src="~assets/images/bot_menu.03_off.jpg" 
+        :class="footerTabName!=='event'?'is-active':''"
+        alt="" srcset=""
+      />
+      <img class="button-image-3 on" 
+        src="~assets/images/bot_menu.03_on.jpg" 
+        :class="footerTabName=='event'?'is-active':''"
+        alt="" srcset=""
+      />
     </div>
-    <div class="footer-button">
-      <img class="button-image-4 off" src="~assets/images/bot_menu.04_off.jpg" alt="" srcset="">
-      <img class="button-image-4 on" src="~assets/images/bot_menu.04_on.jpg" alt="" srcset="">
+    <div class="footer-button" @click="movePage('/center')">
+      <img class="button-image-4 off" 
+        src="~assets/images/bot_menu.04_off.jpg" 
+        :class="footerTabName!='center'?'is-active':''"
+        alt="" srcset=""
+      />
+      <img class="button-image-4 on" 
+        src="~assets/images/bot_menu.04_on.jpg" 
+        :class="footerTabName=='center'?'is-active':''"
+        alt="" srcset=""
+      />
     </div>
-    <div class="footer-button">
-      <img class="button-image-5 off" src="~assets/images/bot_menu.05_off.jpg" alt="" srcset="">
-      <img class="button-image-5 on" src="~assets/images/bot_menu.05_on.jpg" alt="" srcset="">
+    <div class="footer-button" @click="movePage('/settings')">
+      <img class="button-image-5 off" 
+        src="~assets/images/bot_menu.05_off.jpg" 
+        :class="footerTabName!='settings'?'is-active':''"
+        alt="" srcset=""
+      />
+      <img class="button-image-5 on" 
+        src="~assets/images/bot_menu.05_on.jpg" 
+        :class="footerTabName=='settings'?'is-active':''"
+        alt="" srcset=""
+      />
     </div>
   </div>
 </template>
 
 <script>
+import UtilMethodMixin from "../UtilMethodMixin";
 export default {
+  mixins: [UtilMethodMixin],
   name: 'Footer',
   data () {
-    return {}
+    return {
+    }
+  },
+  computed:{
+    footerTabName(){
+      let returnValue = "";
+      if(this.$router.currentRoute.path=="/main"){
+        returnValue = "main";
+      } else if(this.$router.currentRoute.path=="/main"){
+        returnValue = "main";
+      } else if(this.$router.currentRoute.path=="/event"){
+        returnValue = "event";
+      } else if(this.$router.currentRoute.path=="/center"){
+        returnValue = "center";
+      } else if(this.$router.currentRoute.path=="/settings"){
+        returnValue = "settings";
+      }
+      return returnValue
+    }
   }
 }
 </script>
@@ -51,17 +111,20 @@ export default {
       display: flex;
       flex: 1;
       justify-content: flex-end;
-      .on{
+      img{
         display: none;
       }
       &:hover{
         cursor: pointer;
         .on{
-          display: block;
+          display: block ;
         }
         .off{
           display: none;
         }
+      }
+      img.is-active{
+        display: block ;
       }
     }
   }
