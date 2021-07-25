34541f8f8b2c4fadb745d2de1913a213c3fa0a70
sungkuk5420 <sungkuk5420@gmail.com>
2021-07-25 16:02:59 +0900

    메인 페이지 완성

---
+++ src/components/MainContent.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="main-content" >
+    <div class="main-content-image" :style="`background-image:url(${imageData});`">
+    </div>
+    <div class="main-content-text">
+      <div class="content">
+        <div class="content-left">
+          <div class="title">
+            {{data.title}}
+          </div>
+          {{data.content}}
+        </div>
+        <div class="content-right">
+          <div class="content-right-row">
+            <div class="content-right-icon">
+              시간
+            </div>
+            <div class="content-right-text">
+              {{data.time}}
+            </div>
+          </div>
+          <div class="content-right-row">
+            <div class="content-right-icon">
+              비용
+            </div>
+            <div class="content-right-text">
+              {{data.price}}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainContent',
+  props:["data"],
+  data () {
+    return {
+    }
+  },
+  computed: {
+    imageData () {
+      return require('../assets/'+this.data.image)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.main-content{
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin-bottom: 10px;
+    border: 1px solid #bebebe;
+    &:hover{
+      cursor: pointer;
+    }
+    &-image{
+      display: flex;
+      height: 200px;
+      flex:none;
+      background-size: cover;
+      background-position: center center;
+    }
+    &-text{
+      border-top:2px solid #afafaf;
+      border-bottom:2px solid #afafaf;
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      flex:none;
+      background: white;
+      height:56px;
+      .title{
+        color:black;
+        font-weight: bold;
+        font-size: 16px;
+        line-height: 1;
+      }
+      .content{
+        color:#8d8d8d;
+        font-size: 11px;
+        font-weight: bold;
+        display: flex;
+        &-left{
+          padding: 11px 10px 10px 10px;
+          line-height: 2;
+          flex:4;
+          height:50%;
+          font-size: 11px;
+        }
+        &-right{
+          flex:1.8;
+          height:50%;
+          &-row{
+            display: flex;
+            align-items: center;
+            flex:none;
+            margin: 5px 0 4px 0;
+          }
+          &-icon{
+            line-height: 2.1;
+            margin-right: 5px;
+            font-size: 11px;
+            padding: 0px 5px;
+            height: 18px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: white;
+            border-radius: 3px;
+            background: #3d3d3d;
+          }
+        }
+      }
+    }
+  }
+</style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,6 +1,8 @@
 <template>
   <q-page class="column flex justify-start items-center main-page">
-    <Header :centerTab="centerTab"></Header>
+    <div class="scroll-container">
+      <MainContent v-for="(conetent,index) in mainContents" :key="index" :data="conetent"></MainContent>
+    </div>
     <Footer></Footer>
   </q-page>
 </template>
@@ -9,16 +11,35 @@
 import ComputedMixin from "../ComputedMixin";
 import UtilMethodMixin from "../UtilMethodMixin";
 import Footer from "../components/Footer.vue";
-import Header from "../components/Header.vue";
+import MainContent from "../components/MainContent.vue";
 export default {
-  mixins: [ComputedMixin, UtilMethodMixin,Footer],
-  components:{Footer,Header},
+  mixins: [ComputedMixin, UtilMethodMixin],
+  components:{Footer,MainContent},
   data(){
     return{
-      dense:true,
-      loginCheck:false,
-      id:"",
-      password:""
+      mainContents:[
+        {
+          image:"images/home1.jpg",
+          title:"엔진오일 퍼포먼스 장비",
+          content:"필립스 5k 기존 안개등 탈착 후 구조변경 장착...",
+          time:"약 3시간",
+          price:"350,000원",
+        },
+        {
+          image:"images/home2.jpg",
+          title:"타이어 교환 및 관리 정비",
+          content:"스마트월드표 걸윙도어 장착",
+          time:"약 2일",
+          price:"200,000원",
+        },
+        {
+          image:"images/home3.jpg",
+          title:"브레이크 관리 정비",
+          content:"브레이크 패드 교체 작업",
+          time:"약 2시간",
+          price:"100,000원",
+        },
+      ]
     }
   },
   mounted() {
@@ -34,5 +55,12 @@ export default {
   margin-right: auto;
   background:$pageBgColor;
   width: 100%;
+  flex-wrap: nowrap;
+  .scroll-container{
+    width:100%;
+    padding: 10px;
+    padding-bottom: 50px;
+    overflow: auto;
+  }
 }
 </style>
