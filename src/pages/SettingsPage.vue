a4b9c475c14d4547c732df6283046bf30b521dd0
sungkuk5420 <sungkuk5420@gmail.com>
2021-08-01 16:23:04 +0900

    셋팅 페이지 완성

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,41 +1,264 @@
 <template>
   <q-page class="column flex justify-start items-center estimate-page">
-    <div class="header full-width flex">
-      <div class="header-center">
-        차량 관리
-      </div>
-    </div>
-    <div class="content">
-      <div class="menu-row" v-for="(item,index) in menuDatas" :key="index" @click="()=>{action(index)}">
-        <div class="menu-row__left">
-          {{item.label}}
+    <q-carousel
+      v-model="slide"
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      swipeable
+      animated
+      control-color="primary"
+      padding
+      width="100%"
+      height="100%"
+      class="bg-white shadow-2 rounded-borders"
+    >
+      <q-carousel-slide :name="1" class="column no-wrap">
+        <div class="header full-width flex">
+          <div class="header-center">
+            차량 관리
+          </div>
         </div>
-        <div class="menu-row__right" >
-          <q-toggle
-            v-if="index==0"
-            v-model="notification"
-            checked-icon="check"
-            color="negative"
-            unchecked-icon="clear"
-          />
-          <div style="color:#fe2d2a; font-weight:bold;" v-if="index==1">010-1234-5678</div>
-          <q-toggle
-            v-if="index==2"
-            v-model="autoLogin"
-            checked-icon="check"
-            color="negative"
-            unchecked-icon="clear"
-          />
-          
-          <q-btn
-            class="arrow-icon"
-            v-if="(index==5)||(index==6)"
-            :icon="$q.platform.is.ios ? 'arrow_back_ios' : 'arrow_back_ios'"
-            flat
-          />
+        <div class="content">
+          <div class="menu-row relative-position " v-ripple v-for="(item,index) in menuDatas" :key="index" @click="()=>{action(index)}">
+            <div class="menu-row__left">
+              {{item.label}}
+            </div>
+            <div class="menu-row__right" >
+              <q-toggle
+                v-if="index==0"
+                v-model="notification"
+                checked-icon="check"
+                color="negative"
+                unchecked-icon="clear"
+              />
+              <div style="color:#fe2d2a; font-weight:bold;" v-if="index==1">010-1234-5678</div>
+              <q-toggle
+                v-if="index==2"
+                v-model="autoLogin"
+                checked-icon="check"
+                color="negative"
+                unchecked-icon="clear"
+              />
+              
+              <q-btn
+                class="arrow-icon"
+                v-if="(index==5)||(index==6)"
+                :icon="$q.platform.is.ios ? 'arrow_back_ios' : 'arrow_back_ios'"
+                flat
+              />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </q-carousel-slide>
+      <q-carousel-slide :name="2" class="column no-wrap">
+        <div class="header full-width flex">
+          <div class="header-left" @click="slide=1">
+            <q-btn
+              :icon="$q.platform.is.ios ? 'arrow_back_ios' : 'arrow_back_ios'"
+              flat
+            />
+          </div>
+          <div class="header-center">
+            내 글 보기
+          </div>
+        </div>
+        <div class="content list-content" >
+          <q-list >
+            <q-item clickable >
+              <q-item-section class="list-content__image">
+                <img src="~assets/images/car3.jpg" style="width:100px;">
+              </q-item-section>
+              <q-item-section class="list-content__text">
+                <q-item-label>견적 요청합니다.</q-item-label>
+                <q-item-label caption>사고로 이렇게 되었는데 얼마정도 들까요? 사진 첨부합니다.</q-item-label>
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>2분 전</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator spaced inset />
+            <q-item clickable >
+              <q-item-section class="list-content__image">
+                <img src="~assets/images/car3.jpg" style="width:100px;">
+              </q-item-section>
+              <q-item-section class="list-content__text">
+                <q-item-label>견적 요청합니다.</q-item-label>
+                <q-item-label caption>사고로 이렇게 되었는데 얼마정도 들까요? 사진 첨부합니다.</q-item-label>
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>2분 전</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator spaced inset />
+            <q-item clickable >
+              <q-item-section class="list-content__image">
+                <img src="~assets/images/car3.jpg" style="width:100px;">
+              </q-item-section>
+              <q-item-section class="list-content__text">
+                <q-item-label>견적 요청합니다.</q-item-label>
+                <q-item-label caption>사고로 이렇게 되었는데 얼마정도 들까요? 사진 첨부합니다.</q-item-label>
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>2분 전</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator spaced inset />
+            <q-item clickable >
+              <q-item-section class="list-content__image">
+                <img src="~assets/images/car3.jpg" style="width:100px;">
+              </q-item-section>
+              <q-item-section class="list-content__text">
+                <q-item-label>견적 요청합니다.</q-item-label>
+                <q-item-label caption>사고로 이렇게 되었는데 얼마정도 들까요? 사진 첨부합니다.</q-item-label>
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>2분 전</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator spaced inset />
+            <q-item clickable >
+              <q-item-section class="list-content__image">
+                <img src="~assets/images/car3.jpg" style="width:100px;">
+              </q-item-section>
+              <q-item-section class="list-content__text">
+                <q-item-label>견적 요청합니다.</q-item-label>
+                <q-item-label caption>사고로 이렇게 되었는데 얼마정도 들까요? 사진 첨부합니다.</q-item-label>
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>2분 전</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator spaced inset />
+            <q-item clickable >
+              <q-item-section class="list-content__image">
+                <img src="~assets/images/car3.jpg" style="width:100px;">
+              </q-item-section>
+              <q-item-section class="list-content__text">
+                <q-item-label>견적 요청합니다.</q-item-label>
+                <q-item-label caption>사고로 이렇게 되었는데 얼마정도 들까요? 사진 첨부합니다.</q-item-label>
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>2분 전</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator spaced inset />
+          </q-list>
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide :name="3" class="column no-wrap">
+        <div class="header full-width flex">
+          <div class="header-left" @click="slide=1">
+            <q-btn
+              :icon="$q.platform.is.ios ? 'arrow_back_ios' : 'arrow_back_ios'"
+              flat
+            />
+          </div>
+          <div class="header-center">
+            공지사항
+          </div>
+        </div>
+        <div class="content list-content" >
+          <q-list >
+            <q-item clickable >
+              <q-item-section class="list-content__text">
+                <q-item-label>공지사항입니다.</q-item-label>
+                <q-item-label caption>안녕하세요 착한 공업사입니다. 금일 예약손님이 취소되어 오일교체 및 타이어 교체 50% 할인요금에 진행합니다.</q-item-label>
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>2분 전</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator spaced inset />
+            <q-item clickable >
+              <q-item-section class="list-content__text">
+                <q-item-label>공지사항입니다.</q-item-label>
+                <q-item-label caption>안녕하세요 착한 공업사입니다. 금일 예약손님이 취소되어 오일교체 및 타이어 교체 50% 할인요금에 진행합니다.</q-item-label>
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>2분 전</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator spaced inset />
+            <q-item clickable >
+              <q-item-section class="list-content__text">
+                <q-item-label>공지사항입니다.</q-item-label>
+                <q-item-label caption>안녕하세요 착한 공업사입니다. 금일 예약손님이 취소되어 오일교체 및 타이어 교체 50% 할인요금에 진행합니다.</q-item-label>
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>2분 전</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator spaced inset />
+            <q-item clickable >
+              <q-item-section class="list-content__text">
+                <q-item-label>공지사항입니다.</q-item-label>
+                <q-item-label caption>안녕하세요 착한 공업사입니다. 금일 예약손님이 취소되어 오일교체 및 타이어 교체 50% 할인요금에 진행합니다.</q-item-label>
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>2분 전</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator spaced inset />
+            <q-item clickable >
+              <q-item-section class="list-content__text">
+                <q-item-label>공지사항입니다.</q-item-label>
+                <q-item-label caption>안녕하세요 착한 공업사입니다. 금일 예약손님이 취소되어 오일교체 및 타이어 교체 50% 할인요금에 진행합니다.</q-item-label>
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>2분 전</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator spaced inset />
+            <q-item clickable >
+              <q-item-section class="list-content__text">
+                <q-item-label>공지사항입니다.</q-item-label>
+                <q-item-label caption>안녕하세요 착한 공업사입니다. 금일 예약손님이 취소되어 오일교체 및 타이어 교체 50% 할인요금에 진행합니다.</q-item-label>
+              </q-item-section>
+
+              <q-item-section side top>
+                <q-item-label caption>2분 전</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator spaced inset />
+          </q-list>
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
+
+    <q-dialog v-model="programInfoPopup">
+      <q-card>
+        <q-toolbar style="border-bottom:1px solid #ddd">
+          <q-toolbar-title style="padding:15px 0 0 0;">
+            <img src="~assets/images/logo.png" style="width:60%;">
+          </q-toolbar-title>
+
+          <q-btn flat round dense icon="close" v-close-popup />
+        </q-toolbar>
+
+        <q-card-section>
+           부천에 위치한 착한 자동차 공업사 고객관리 앱입니다. 
+           <br>
+           <br>
+           대표: 이광현
+           <br>
+           위치 : 원미동 777-30번지 부천시 경기도
+           <br>
+           전화번호 : 010-1234-5678
+           <br>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
     <Footer></Footer>
   </q-page>
 </template>
@@ -49,6 +272,8 @@ export default {
   components:{Footer},
   data(){
     return{
+      slide:1,
+      programInfoPopup:false,
       notification:true,
       autoLogin:false,
       menuDatas:[
@@ -86,6 +311,15 @@ export default {
         case 3:
           this.movePage("/")
           break;
+        case 5:
+          this.slide = 2;
+          break;
+        case 6:
+          this.slide = 3;
+          break;
+        case 7:
+          this.programInfoPopup = true;
+          break;
       
         default:
           break;
@@ -109,6 +343,9 @@ export default {
   padding-bottom:57px;
   align-items: center;
   justify-content: flex-start;
+  .q-carousel{
+    width: 100%;
+  }
   .header{
     z-index: 1;
     background: white;
@@ -155,7 +392,6 @@ export default {
     align-items: center;
     height:45px;
     &:hover{
-      background:#ddd;
       cursor: pointer;
     }
     &:focus,
@@ -211,6 +447,34 @@ export default {
       padding-right: 5px;
       padding-left: 15px;
     }
+  }
+  .q-carousel__slide{
+    padding: 0;
+  }
+  .content.list-content{
+    background: white;
+    padding:0; 
+    padding-top:10px;
+  }
+  .q-list{
+    padding: 0;
+  }
+  .q-item{
+    background: white;
+    padding: 15px 10px;
+  }
+  .q-separator--horizontal-inset{
+    margin: 0 !important;
+  }
+  .q-item__section,
+  .list-content__image{
+    flex:1;
+  }
+  .list-content__image{
+    flex:2;
+  }
+  .list-content__text{
+    flex:5;
   }
 }
 </style>
